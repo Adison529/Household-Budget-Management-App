78edf1bb036ddbcbe7792aa148c17ffe9d428a98
Adison529 <102924511+Adison529@users.noreply.github.com>
2024-06-19 22:01:22 +0200

    Deploy v0.1
    front & back

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AppNavbar from './components/Navbar';
+import BudgetOperations from './components/BudgetList';  // Zmieniono import na BudgetOperations
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  const token = localStorage.getItem('token');
+
+  return (
+    <Router>
+      <div className="App">
+        <AppNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/budget-managers/:budget_manager_id/operations" element={token ? <BudgetOperations /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
